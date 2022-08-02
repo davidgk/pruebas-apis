4afb3421a9d42f7136956e87b90d2300f0612c18
David Kotlirevsky <david.kotlirevsky@gmail.com>
2022-08-02 19:26:51 -0300

    Create api products

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,23 @@
+
+
+
+
+export const getProducts = (req, res) => {
+    return res.json('get Products');
+}
+
+export const createProduct = (req, res) => {
+    return res.json('getProducts');
+}
+
+export const getProductById = (req, res) => {
+
+}
+
+export const updateProducts = (req, res) => {
+
+}
+
+export const deleteProducts = (req, res) => {
+
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,7 +1,18 @@
 import {Router} from "express";
+import {
+    createProduct,
+    deleteProducts,
+    getProductById,
+    getProducts,
+    updateProducts
+} from "../controllers/products.controller";
 
 const productRoutes = Router()
 
-productRoutes.get('/', (req, res) => res.json('getProducts'))
+productRoutes.get('/', getProducts)
+productRoutes.put('/:productId', updateProducts)
+productRoutes.delete('/:productId', deleteProducts)
+productRoutes.get('/:productId', getProductById)
+productRoutes.post('/', createProduct)
 
 export default productRoutes;
