0e246c939b68d0230454dd57da75218aec02180f
David Kotlirevsky <david.kotlirevsky@gmail.com>
2022-08-02 20:06:15 -0300

    create product and find

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,13 +1,17 @@
+import Product from "../models/Product";
 
 
-
-
-export const getProducts = (req, res) => {
-    return res.json('get Products');
+export const getProducts = async (req, res) => {
+    const products = await Product.find();
+    res.json(products)
 }
 
-export const createProduct = (req, res) => {
-    return res.json('getProducts');
+export const createProduct =async  (req, res) => {
+    console.log(req.body)
+    const {name, category, imgUrl, price} = req.body;
+    const newProduct = new Product({name, category, imgUrl, price});
+    const prodSaved = await newProduct.save();
+    res.status(201).json(prodSaved);
 }
 
 export const getProductById = (req, res) => {
