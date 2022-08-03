7c4b33f80811875f593fde2d17ec1f3eadaed469
David Kotlirevsky <david.kotlirevsky@gmail.com>
2022-08-03 17:44:17 -0300

    user sign up with token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken"
+import config from "../config";
 export const signoff= async (req, res) => {
     res.json('signoff')
 }
@@ -9,17 +10,21 @@ export const signin  = async (req, res) => {
 }
 
 export const signup  = async (req, res) => {
-    const {username , email, password, roles} = req.body
-    const newUser = new User({username,
-        email,
-        password: await User.encryptPassword(password),
-        roles});
-    const userSaved = await newUser.save();
+    try {
+        const {username , email, password, roles} = req.body
+        const newUser = new User({username,
+            email,
+            password: await User.encryptPassword(password),
+            roles});
+        const userSaved = await newUser.save();
 
-    const whatImSavingInToken = { id: userSaved._id}
-    const privateKey = '';
-    const configurationObject = {};
-    const jwtSaved = jwt.sign(whatImSavingInToken, privateKey, configurationObject)
-    res.status(201).json(userSaved);
+        const whatImSavingInToken = { id: userSaved._id}
+        const privateKey = config.PRODUCT_API;
+        const configurationObject = { expiresIn: 86400};
+        const token = jwt.sign(whatImSavingInToken, privateKey, configurationObject)
+        res.status(201).json({token})
+    } catch (e) {
+        res.status(400).json({error:e})
+    }
 
 }
