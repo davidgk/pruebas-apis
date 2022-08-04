a335099d9e7b4bb7096af06e4a90748563c60712
David Kotlirevsky <david.kotlirevsky@gmail.com>
2022-08-04 14:11:59 -0300

    complte validation for token

--- src/middlewares/authjwt.js
+++ src/middlewares/authjwt.js
@@ -1,10 +1,18 @@
 import jwt from "jsonwebtoken"
 import config from "../config";
+import User from "../models/User";
 export const verifyToken = async (req, res, next) => {
     const token = req.headers['x-api-key']
     if(!token) return res.status(403).json({message: 'No token provided'})
-    const decoded = jwt.verify(token, config.PRODUCT_API);
-    console.log(decoded)
+    try {
+        console.log(token)
+        const decoded = jwt.verify(token, config.PRODUCT_API);
+        const userExists = await User.findById(decoded.id)
+        if( !userExists) return res.status(404).json({message: 'User not exists'})
+    } catch (e) {
+        console.log(e)
+        return res.status(403).json({message: 'Authentication Fail: Sign in again'})
+    }
     next()
-    
+
 }
